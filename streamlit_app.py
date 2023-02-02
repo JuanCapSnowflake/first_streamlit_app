998c8710fa34fec83e9165814f5baa8f51ef27dc
JuanCapSnowflake <123999083+JuanCapSnowflake@users.noreply.github.com>
2023-02-02 17:34:20 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,27 +26,36 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
-# create a function
+#Create the repeattable code block (called a funcion)
 def get_fruityvice_data(this_fruit_choice):
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)    
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())    
     return fruityvice_normalized
+    
+    
+streamlit.header("Fruityvice Fruit Advice!")
 
-# new section to display fruityvice api response
-streamlit.header('Fruityvice Fruit Advice')
 try:
-    fruit_choice = streamlit.text_input('What fruit would you like information about?')
-    if not fruit_choice:
-        streamlit.error("Please select a fruit to get information.")
-    else:
-        back_from_function = get_fruityvice_data(fruit_choice)
-        streamlit.dataframe(back_from_function) 
-
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.") 
+  else:
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
+    
 except URLError as e:
-    streamlit.error()
+  streamlit.error()
  
-
 streamlit.stop()
+
+#streamlit.text(fruityvice_response.json())
+
+# write your own comment -what does the next line do? 
+
+# write your own comment - what does this do?
+
+
+
 
 #streamlit.text(fruityvice_response.json())
 
