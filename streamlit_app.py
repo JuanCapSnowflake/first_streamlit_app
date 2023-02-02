f83fe222e1b3676b8f0f68a72a3c51109eb51514
JuanCapSnowflake <123999083+JuanCapSnowflake@users.noreply.github.com>
2023-02-02 15:45:45 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,12 @@
-import streamlit
-import pandas
-import requests
-import snowflake.connector
+#import streamlit
+#import pandas
+#import requests
+#import snowflake.connector
+from urllib.error import URLError
+
+streamlit.stop()
+
+
 
 
 streamlit.title('My Parents New Healthy Diner')
